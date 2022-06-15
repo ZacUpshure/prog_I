add81720c29209cdf7f91780dfcb852d06a38866
Zac <zac.upshure@outlook.com>
2022-06-15 13:22:36 +0200

    Add files via upload

---
+++ Aufgabe7/src/Envelope.java
@@ -0,0 +1,211 @@
+public class Envelope {
+
+    public static int width = 60;
+    public static int border = 3;       // border must be 3
+    public static int padding = 3;
+    public static int hight = 9;
+
+    public static String spaceChar = " ";
+    //public static String altSpaceChar = " ";
+    public static String borderHorizontalChar = "-";
+    public static String borderVerticalChar = "|";
+    public static String cornerChar = "+";
+    public static String lineBreak = "\n";
+
+    public static String fromName = "BHT Berlin";
+    public static String fromAddress = "Luxemburger Str. 10";
+    public static String fromZipCodeAndCity = "13353 Berlin";
+
+    public void printEnvelope(Student[] toStudents) {
+        for(int i=0; i < toStudents.length; i++){
+            //print headerline
+            barFunction();
+            //print emptyline
+            emptyLine();
+            //print senderdata
+            senderData(fromName, fromAddress, fromZipCodeAndCity);
+            //print emptyline
+            emptyLine();
+            //print rescieverdata
+            rescieverData(
+                    toStudents[i].getPrename()+" "+toStudents[i].getSurname(),
+                    toStudents[i].getStreet()+" "+toStudents[i].getHouseNumber(),
+                    toStudents[i].getZipCode()+" "+toStudents[i].getCity()
+            );
+            //print emptyline
+            emptyLine();
+            //print bottomline
+            barFunction();
+        }
+    }
+
+    public static void printEnvelope() {
+
+        printEnvelope("$NAME", "$ADDRESS", "$ZIP $CITY", "$NAME", "$ADDRESS", "$ZIP $CITY");
+    }
+
+    public static void printEnvelope(String fromName, String toName) {
+        printEnvelope(fromName, "$ADDRESS", "$ZIP $CITY", toName, "$ADDRESS", "$ZIP $CITY");
+    }
+
+    public static void printEnvelope(String fromName, String fromAddress, String toName, String toAddress) {
+        printEnvelope(fromName, fromAddress, "$ZIP $CITY", toName, toAddress, "$ZIP $CITY");
+    }
+
+
+    public static void printEnvelope(String fromName, String fromAddress, String fromZipCodeAndCity, String toName, String toAddress, String toZipCodeAndCity ) {
+        //print headerline
+        barFunction();
+        //print emptyline
+        emptyLine();
+        //print senderdata
+        senderData(fromName, fromAddress, fromZipCodeAndCity);
+        //print emptyline
+        emptyLine();
+        //print rescieverdata
+        rescieverData(toName, toAddress, toZipCodeAndCity);
+        //print emptyline
+        emptyLine();
+        //print bottomline
+        barFunction();
+    }
+
+    //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    //%%%%%%% SUB METHOD DEFINITIONS %%%%%%%%%%%
+    //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+    //Padding
+    public static void paddingLoop(){
+        for (int k = 0; k < padding; k++){                                        // print blank " " as much as padding
+            System.out.print(spaceChar/*+spaceChar*/);
+        }
+    }
+
+    //Empty for resciever
+    public static void emptyTo(String Input){
+
+        int lengthOfInput = Input.length();
+        //int reversePadding = padding*2;
+
+        for (int k = 0; k <= width-border-padding-lengthOfInput; k++){            // print blank " " until edge
+            System.out.print(spaceChar);
+        }
+    }
+
+    //Empty for sender
+    public static void emptyFrom(String Input){
+
+        int lengthOfInput = Input.length();
+        //int reversePadding = padding*2;
+
+        for (int k = 0; k <= width-/*2*/border-padding-lengthOfInput; k++){        // print blank " " until edge
+            System.out.print(spaceChar);                                           // border must be 3
+        }
+    }
+
+    //Empty line
+    public static void emptyLine(){
+
+        System.out.print(borderVerticalChar);
+        for (int k = 0; k <= width-border; k++){                                   // print blank " " for intire line
+            System.out.print(spaceChar/*+spaceChar*/);
+        }
+        System.out.print(borderVerticalChar);
+        System.out.print(lineBreak);
+    }
+
+    //Bar
+    public static void barFunction(){
+        System.out.print(cornerChar);                                              // print corner at beginning.
+
+        for(int i=0; i <= width-border; i++){                                      // print as many horizontal lines as 'width - border',
+            // to keep the same width including the cornerChar's.
+
+            System.out.print(borderHorizontalChar);
+        }
+        System.out.println(cornerChar);                                            // print corner at bottom.
+    }
+
+    //to get the longest String
+    public static String getLongestString(String[] array) {                        // compares the two str. and returns the largest one
+        int maxLength = 0;                                                           // initialize maxLenght as counter
+        String longestString = null;                                                 // initialize a variable for the return value
+        for (String s : array) {                                                     // extended for loop for longest string
+            if (s.length() > maxLength) {
+                maxLength = s.length();                                              // allocating max lenght to the length of s
+                longestString = s;                                                   // allocating longestString to s
+            }
+        }
+        return longestString;
+    }
+    //fill the rest of the empty space to the lenght of the largest str.
+    public static void fillToData(String toBeFilled, String fill){
+        int lenght1 = toBeFilled.length();                                         // initializing lenght of the smaller str.
+        int lenght2 = fill.length();                                               // initializing lenght of the largest str.
+        int difference = lenght2-lenght1;                                          // initializing difference of both length
+
+        for(int i=0; i<=difference-1; i++){                                        // printing as many spaceChar's as the differnece
+            System.out.print(spaceChar);
+        }
+    }
+
+    public static void senderData(String fromName, String fromAddress, String fromZipCodeAndCity){
+        //%%%%%%%%% NAME %%%%%%%%%%%%%%
+        System.out.print(borderVerticalChar);     // println is must be only print in loop[1].
+        paddingLoop();                            // add padding.
+        System.out.print(fromName);               // print information.
+        emptyFrom(fromName);                      // create empty space (using String to optimize formatting).
+        System.out.print(borderVerticalChar);     // print pipe.
+        System.out.print(lineBreak);
+
+        //%%%%%%%%% ADDRESS %%%%%%%%%%%%%%
+        System.out.print(borderVerticalChar);     //  ... same as top description.
+        paddingLoop();
+        System.out.print(fromAddress);
+        emptyFrom(fromAddress);
+        System.out.print(borderVerticalChar);
+        System.out.print(lineBreak);
+
+        //%%%%%%%%% ZipCode %%%%%%%%%%%%%%
+        System.out.print(borderVerticalChar);     //  ... same as top description.
+        paddingLoop();
+        System.out.print(fromZipCodeAndCity);
+        emptyFrom(fromZipCodeAndCity);
+        System.out.print(borderVerticalChar);
+        System.out.print(lineBreak);
+    }
+
+    public static void rescieverData(String toName, String toAddress, String toZipCodeAndCity){
+
+        //%%%%%%%%% length of the largest array %%%%%%%%%%%%%%
+        String[] InputStrings = {toName, toAddress, toZipCodeAndCity};
+        String largestString = getLongestString(InputStrings);
+
+        //%%%%%%%%% NAME %%%%%%%%%%%%%%
+        System.out.print(borderVerticalChar);      //  ... same as top description.
+        emptyTo(largestString);
+        System.out.print(toName);
+        fillToData(toName, largestString);
+        paddingLoop();
+        System.out.print(borderVerticalChar);
+        System.out.print(lineBreak);
+
+        //%%%%%%%%% ADDRESS %%%%%%%%%%%%%%
+        System.out.print(borderVerticalChar);       //  ... same as top description.
+        emptyTo(largestString);
+        System.out.print(toAddress);
+        fillToData(toAddress, largestString);
+        paddingLoop();
+        System.out.print(borderVerticalChar);
+        System.out.print(lineBreak);
+
+        //%%%%%%%%% ZipCode %%%%%%%%%%%%%%
+        System.out.print(borderVerticalChar);       //  ... same as top description.
+        emptyTo(largestString);
+        System.out.print(toZipCodeAndCity);
+        fillToData(toZipCodeAndCity, largestString);
+        paddingLoop();
+        System.out.print(borderVerticalChar);
+        System.out.print(lineBreak);
+    }
+}
---
+++ Aufgabe7/src/Student.java
@@ -0,0 +1,54 @@
+public class Student {
+    private String prename = "";
+    private String surname = "";
+    private String street = "";
+    private String houseNumber = "";
+    private String zipCode = "";
+    private String city = "";
+    private String registrationNumber = "";
+    private String course = "";
+
+    public Student(String prename, String surname, String street, String houseNumber, String zipCode, String city, String registrationNumber, String course) {
+
+        this.prename = prename;
+        this.surname = surname;
+        this.street = street;
+        this.houseNumber = houseNumber;
+        this.zipCode = zipCode;
+        this.city = city;
+        this.registrationNumber = registrationNumber;
+        this.course = course;
+    }
+
+    public String getPrename(){
+        return prename;
+    }
+
+    public String getSurname(){
+        return surname;
+    }
+
+    public String getStreet(){
+        return street;
+    }
+
+    public String getHouseNumber(){
+        return houseNumber;
+    }
+
+    public String getZipCode(){
+        return zipCode;
+    }
+
+    public String getCity(){
+        return city;
+    }
+
+    public String getRegistrationNumber(){
+        return registrationNumber;
+    }
+
+    public String getCourse(){
+        return course;
+    }
+}
