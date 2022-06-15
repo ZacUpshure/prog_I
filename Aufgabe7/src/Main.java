374947d12f85ac817db9f932542222e4f53ed3e3
Zac <zac.upshure@outlook.com>
2022-06-15 13:43:16 +0200

    Update Main.java

--- Aufgabe7/src/Main.java
+++ Aufgabe7/src/Main.java
@@ -1,15 +1,17 @@
 public class Main {
 
     public static void main(String[] args) {
-        Envelope envelope = new Envelope();
+        
+        Envelope envelope = new Envelope(); //call constructor.
 
-        Student s1 = new Student("John", "Doe", "Doestreet", "42", "12212", "Fancytown", "2323232", "Medieninformatik");
+        //array of objects / array of refernce types.
+        Student s1 = new Student("John", "Doe", "Doestreet", "42", "12212", "Fancytown", "2323232", "Medieninformatik");    
         Student s2 = new Student("Bessie", "Clemente", "Wilson Avenue", "40", "75081", "Texas", "63453", "Medieninformatik");
 
-        envelope.printEnvelope(new Student[] { s1 });
+        envelope.printEnvelope(new Student[] { s1 });   //reference to s1 of Student class.
 
         System.out.println("--------------------------------------");
 
-        envelope.printEnvelope(new Student[] { s1, s2 });
+        envelope.printEnvelope(new Student[] { s1, s2 });   //reference to both s1, s2 of Student class.
     }
 }
